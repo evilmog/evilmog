63e277b4e100585c1a4529a48b8c5681f968b9bd
evilmog <evilmog@evilmogs-MacBook-Pro.local>
2017-11-27 12:53:27 -0700

    update

--- powershell.ps1
+++ powershell.ps1
@@ -1,11 +1,20 @@
 # Mogs collection of powershell crap
+Write-Host "Powershell Transcription Logging:"
 [Bool](Get-ItemProperty 'HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\PowerShell\Transcription' -ErrorAction SilentlyContinue).EnableTranscripting
+Write-Host "Powershell Module Logging:"
 [Bool](Get-ItemProperty 'HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\Power Shell\ModuleLogging' -ErrorAction SilentlyContinue).EnableModuleLogging
+Write-Host "Script Block Logging:"
 [Bool](Get-ItemProperty 'HKLM:\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\Power Shell\ScriptBlockLogging' -ErrorAction SilentlyContinue).EnableScriptBlockLogging
+Write-Host "Powershell Version"
 (Get-ItemProperty HKLM:\SOFTWARE\Microsoft\PowerShell*\PowerShellEngine -Name PowerShellVersion).PowerShellVersion
+Write-Host "Powershell 2 Enabled:"
 (Test-Path $env:windir\Microsoft.Net\Framework\v2.0.50727\System.dll)
+Write-Host "Powershell 5 Enabled:"
 (Test-Path $env:windir\Microsoft.Net\Framework\v4.0.30319\System.dll)
+Write-Host "Legacy Scripting Enabled:"
 [Bool](Get-ItemProperty 'HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\Configuration' -ErrorAction SilentlyContinue).EnableAt
+Write-Host "Machine Password Change Disabled:"
 (Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Services\Netlogon\Parameters).DisablePasswordChange
+Write-Host "Maximum Machine Password Age:"
 (Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Services\NetLogon\Parameters).MaximumPasswordAge
 
